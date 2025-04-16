3c37a34df114648133cd3b72c175aac986daed75
NineLord <35367897+NineLord@users.noreply.github.com>
2025-04-16 22:57:03 +0300

    Feature backup failed to parse configs (#9)
    * Added flag to indicate if parsing the config failed
    
    * Backing up prev config file if failed to parse it when need to overwrite it

--- src/sub_commands/config/file_handler.rs
+++ src/sub_commands/config/file_handler.rs
@@ -1,12 +1,13 @@
-use std::{fs, path::PathBuf};
+use std::{time::{SystemTime, UNIX_EPOCH}, fs, path::{Path, PathBuf}};
 use anyhow::{Context, Result};
-use log::{info, warn};
+use log::info;
 use ron::{ser::PrettyConfig, de::from_reader};
 use tap::Tap;
 use super::structure::Config;
 use crate::{GlobalOptions, utils::default_value_home_dir};
 
 pub struct FileHandler;
+const CONFIG_FILE_NAME: &str = ".shimirc.ron";
 
 // Default values
 impl FileHandler {
@@ -19,8 +20,6 @@ impl FileHandler {
 	/// # Errors
 	/// * Failed to get the path to the home directory of the user.
 	fn get_config_file_path() -> Result<PathBuf> {
-		const CONFIG_FILE_NAME: &str = ".shimirc.ron";
-
 		Ok(
 			default_value_home_dir()?
 				.tap_mut(|home_dir| home_dir.push(CONFIG_FILE_NAME))
@@ -34,21 +33,13 @@ impl FileHandler {
 	/// * Failed to get the path to the config file.
 	/// * Failed to open the config file.
 	/// * Failed to deserialize the config file.
-	pub(crate) fn read(version: &str) -> Result<Config> {
+	pub(crate) fn read() -> Result<Option<Config>> {
 		let config_path = Self::get_config_file_path()?;
 
 		let config_file = fs::File::open(&config_path)
 			.with_context(|| format!("Failed to open the config file at: {config_path:?}"))?;
 
-		let config: Config = from_reader(config_file)
-			.with_context(|| {
-				warn!("Failed to parse previous config file.
-could it be from previous versions of the tool? (Current version: {version:?})
-Continuing with default config."); // No backward support as of yet.
-				format!("Failed to deserialize the config file at: {config_path:?}")
-			})?;
-
-		Ok(config)
+		Ok(from_reader(config_file).ok())
 	}
 
 	/// # Errors
@@ -59,6 +50,11 @@ Continuing with default config."); // No backward support as of yet.
 		let config_path = Self::get_config_file_path()?;
 
 		if !cfg!(feature = "dry_run") {
+			if global_options.is_fail_to_parse_config {
+				let prev_config_path = Self::backup_prev_config(&config_path)?;
+				info!("Previous config file was backed up due to failure to parse it at: {prev_config_path:?}");
+			}
+
 			let config_file = fs::OpenOptions::new()
 				.create(true)
 				.truncate(true)
@@ -77,5 +73,23 @@ Continuing with default config."); // No backward support as of yet.
 			info!("Config file updated");
 		}
 		Ok(())
+	}
+
+	fn backup_prev_config(config_path: &PathBuf) -> Result<PathBuf> {
+		let new_config_file_name = {
+			let config_file_name = Path::new(CONFIG_FILE_NAME);
+			let config_file_basename = config_file_name.file_stem().and_then(|x| x.to_str()).unwrap();
+			let config_file_extension = config_file_name.extension().and_then(|x| x.to_str()).unwrap();
+			format!("{config_file_basename}_{}.{config_file_extension}",
+				SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis())
+		};
+		
+		let mut new_config_path = config_path.clone();
+		new_config_path.pop(); // Removes `CONFIG_FILE_NAME`
+		new_config_path.push(new_config_file_name);
+
+		fs::rename(config_path, &new_config_path)?;
+
+		Ok(new_config_path)
 	}
 }
