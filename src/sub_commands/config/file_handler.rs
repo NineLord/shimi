fb19a9e2193595bdda7111143c2f63227c979d82
NineLord <eightlord@gmail.com>
2025-04-16 22:55:42 +0300

    Backing up prev config file if failed to parse it when need to overwrite it

--- src/sub_commands/config/file_handler.rs
+++ src/sub_commands/config/file_handler.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf};
+use std::{time::{SystemTime, UNIX_EPOCH}, fs, path::{Path, PathBuf}};
 use anyhow::{Context, Result};
 use log::info;
 use ron::{ser::PrettyConfig, de::from_reader};
@@ -7,6 +7,7 @@ use super::structure::Config;
 use crate::{GlobalOptions, utils::default_value_home_dir};
 
 pub struct FileHandler;
+const CONFIG_FILE_NAME: &str = ".shimirc.ron";
 
 // Default values
 impl FileHandler {
@@ -19,8 +20,6 @@ impl FileHandler {
 	/// # Errors
 	/// * Failed to get the path to the home directory of the user.
 	fn get_config_file_path() -> Result<PathBuf> {
-		const CONFIG_FILE_NAME: &str = ".shimirc.ron";
-
 		Ok(
 			default_value_home_dir()?
 				.tap_mut(|home_dir| home_dir.push(CONFIG_FILE_NAME))
@@ -51,6 +50,11 @@ impl FileHandler {
 		let config_path = Self::get_config_file_path()?;
 
 		if !cfg!(feature = "dry_run") {
+			if global_options.is_fail_to_parse_config {
+				let prev_config_path = Self::backup_prev_config(&config_path)?;
+				info!("Previous config file was backed up due to failure to parse it at: {prev_config_path:?}");
+			}
+
 			let config_file = fs::OpenOptions::new()
 				.create(true)
 				.truncate(true)
@@ -69,5 +73,23 @@ impl FileHandler {
 			info!("Config file updated");
 		}
 		Ok(())
+	}
+
+	fn backup_prev_config(config_path: &PathBuf) -> Result<PathBuf> {
+		let new_config_file_name = {
+			let config_file_name = Path::new(CONFIG_FILE_NAME);
+			let config_file_basename = config_file_name.file_stem().and_then(|x| x.to_str()).unwrap();
+			let config_file_extension = config_file_name.extension().and_then(|x| x.to_str()).unwrap();
+			format!("{config_file_basename}_{}.{config_file_extension}",
+				SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis())
+		};
+		
+		let mut new_config_path = config_path.clone();
+		new_config_path.pop(); // Removes `CONFIG_FILE_NAME`
+		new_config_path.push(new_config_file_name);
+
+		fs::rename(config_path, &new_config_path)?;
+
+		Ok(new_config_path)
 	}
 }
