942613c8e66c6d9fba01319a1597f2cba567f9ab
NineLord <eightlord@gmail.com>
2025-04-18 07:41:48 +0300

    Impl basic ip

--- src/sub_commands/orchestration/ip.rs
+++ src/sub_commands/orchestration/ip.rs
@@ -1,49 +1,30 @@
 use anyhow::Result;
-use clap::{Args, ArgAction::SetFalse};
+use clap::Args;
 use super::{command::RunOrchestration, global_orch_options::GlobalOrchOptions};
 use crate::{utils::RunCommand, sub_commands::config::OrchestrationType};
 
-/// Fetch the logs of a container
+/// Gets the container's IP
 #[derive(Args, Debug)]
-#[command(visible_aliases = ["l", "log"])]
 pub struct Arguments {
-	/// The name of the container that going to show his logs
+	/// The name of the container that going to give his IP
 	pub container_name: String,
-
-	/// Do not follow log output
-	#[arg(long = "no-follow", action = SetFalse)]
-	pub follow: bool,
-
-	/// Number of lines to show from the end of the logs
-	#[arg(short, long)]
-	pub tail: Option<usize>,
-
 }
 
 impl RunOrchestration for Arguments {
 	fn run_orch(self, global_options: &GlobalOrchOptions) -> Result<()> {
 		let container_name = global_options.get_container_name(&self.container_name)?;
 
-		let mut arguments = vec!["logs", &container_name];
-		if self.follow {
-			arguments.push("--follow");
-		}
-
-		let tail = self.tail
-			.map(|num| num.to_string());
-		if let Some(tail) = &tail {
-			arguments.push("--tail");
-			arguments.push(tail);
-		}
-
-		match global_options.get_orchestration_type() {
+		let _output = match global_options.get_orchestration_type() {
 			OrchestrationType::DockerCompose => {
-				RunCommand::exec_with_args("docker", arguments)
+				RunCommand::run_with_args_sync("docker", ["inspect", &container_name])
 			},
 			OrchestrationType::Kubernetes => {
+				let mut arguments = vec!["TODO", &container_name];
 				global_options.add_name_space(&mut arguments);
-				RunCommand::exec_with_args("oc", arguments)
+				RunCommand::run_with_args_sync("oc", arguments)
 			},
-		}
+		}?;
+
+		todo!("Either parse the given output or display it right away with exec instead of run_sync")
 	}
 }
